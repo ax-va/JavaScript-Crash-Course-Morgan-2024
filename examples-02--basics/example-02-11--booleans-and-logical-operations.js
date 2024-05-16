59697fc2a9bc955b44b2cb619b0017b5c8d960d9
ax-va <vasiliev.informatik@t-online.de>
2024-05-16 21:57:05 +0200

    update

---
+++ examples-02--basics/example-02-11--booleans-and-logical-operations.js
@@ -0,0 +1,26 @@
+let playing;
+// undefined
+
+playing = true; // in Python: True
+// true
+
+playing = false; // in Python: False
+// false
+
+// AND: && // in Python: and
+// OR: || // in Python: or
+// NOT: ! // in Python: not
+
+let powerup = true;
+// undefined
+let jumping = false;
+// undefined
+
+powerup && jumping;
+// false
+
+powerup || jumping;
+// true
+
+!powerup
+// false
