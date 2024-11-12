a42e1f5bd8cfb8df1af849cc5547159af7ac431d
ax-va <vasiliev.informatik@t-online.de>
2024-11-12 19:38:05 +0100

    update

--- 16--d3_js/16-7--barchart/script.js
+++ 16--d3_js/16-7--barchart/script.js
@@ -8,6 +8,8 @@ The bar graph will update as new text is typed or pasted into the box.
 const width = 600;
 const height = 600;
 
+let margin = {top: 20, right: 10, bottom: 20, left: 50};
+
 // Add an svg element to the page
 let svg = d3
     .select("body")
@@ -17,19 +19,39 @@ let svg = d3
 
 function update(data) {
     /*
-    Creates / updates a horizontally oriented bar chart
+    Creates / updates a horizontally oriented, scaled bar chart.
      */
+
+    // to linearly map input values to output values
+    let xScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, d => d.count)])
+        .range([margin.left, width - margin.right]);
+
+    // to create a set of evenly spaced bands
+    let yScale = d3
+        .scaleBand()
+        .domain(data.map(d => d.char))
+        .range([margin.top, height - margin.bottom])
+        // Define how much space there is between bars based on the space available:
+        // 0 means that they are as tall as possible and will be touching,
+        // 0.5 means that the bars will take up half of the space available.
+        .padding(0.5);
+
     svg
         .selectAll("rect")
         .data(data)
         .join("rect")
-        .attr("width", (d, i) => d.count * 5)
-        .attr("height", 10)
-        .attr("x", 20)
-        .attr("y", (d, i) => i * 20);
+        // `xScale(0)` returns the horizontal position of the left side of the current bar.
+        // `xScale(d.count)` returns the horizontal position of the right side of the current bar.
+        .attr("width", (d, i) => xScale(d.count) - xScale(0)) // `.attr("width", (d, i) => d.count * 5)` to set fixed
+        .attr("height", yScale.bandwidth()) // `.attr("height", 10)` to set fixed
+        .attr("x", xScale(0)) // `.attr("x", 20)` to set fixed
+        .attr("y", (d, i) => yScale(d.char)); // `.attr("y", (d, i) => i * 20)` to set fixed
 }
 
-d3.select("textarea")
+d3
+    .select("textarea")
     // The "input" event is triggered anytime the content of the text area changes
     .on("input", (e) => {
         // object for keeping track of the character frequencies
@@ -57,6 +79,6 @@ d3.select("textarea")
         data.sort((a, b) => d3.ascending(a.char, b.char));
         // See in the console that everything is working as expected
         console.log(data);
-        // Create / update a horizontally oriented bar chart
+        // Create / update a horizontally oriented, scaled bar chart
         update(data);
-});
+    });
