555be4f60da06a5cf6beed98cda730b35d41ce35
ax-va <vasiliev.informatik@t-online.de>
2024-11-15 11:06:33 +0100

    update

--- 07--regular-expressions/07-1--regular-expressions.js
+++ 07--regular-expressions/07-1--regular-expressions.js
@@ -16,11 +16,17 @@
 /^hi$/.test("him");
 // false
 
-// Check if the strings start with a character in the range [A-Z]
-// and end with a next character in the range [a-z].
+// Check if the strings start with an uppercase character in the range [A-Z]
+// and end with a next lowercase character in the range [a-z].
 /^[A-Z][a-z]$/.test("Hi");
 // true
 /^[A-Z][a-z]$/.test("iH");
 // false
 /^[A-Z][a-z]$/.test("Hip");
+// false
+
+// Check matching a single digit
+/^[0-9]$/.test("1");
+// true
+/^[0-9]$/.test("123");
 // false
